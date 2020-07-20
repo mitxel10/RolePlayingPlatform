3696b49fbc7464b139219a7a92e7ca37d1da7d0d
unai <unai.mitxelena@opendeusto.es>
2020-07-20 11:41:40 +0200

    añadida vista de perfil del usuario

--- src/app/tabsPrincipal/perfil/perfil.page.ts
+++ src/app/tabsPrincipal/perfil/perfil.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'firebase';
+import { UsuariosService } from 'src/app/services/usuarios-service/usuarios.service';
+import { AuthenticationService } from 'src/app/services/authentication-service/authentication.service';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilPage implements OnInit {
 
-  constructor() { }
+  public usuario: User;
+
+  constructor(public authService: AuthenticationService, private usuariosService: UsuariosService) { }
 
   ngOnInit() {
+    this.usuario = JSON.parse(localStorage.getItem('user'));
+
+    this.usuariosService.getDatosUsuario(this.usuario.uid).subscribe(usuarioBuscado => {
+      this.usuario = usuarioBuscado.data() as User;
+    });
   }
 
 }
